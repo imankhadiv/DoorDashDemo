0bf25b73442b0f93ea5388ab9ab0a1ce9f0e361b
iman <iman.khadiv@gmail.com>
2017-10-13 03:02:30 -0700

    added recyclerView to main activity

--- app/src/main/java/elrast/com/doordashapp/activity/MainActivity.java
+++ app/src/main/java/elrast/com/doordashapp/activity/MainActivity.java
@@ -6,10 +6,13 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,13 +22,14 @@ import elrast.com.doordashapp.R;
 import elrast.com.doordashapp.api.DoorDashApi;
 import elrast.com.doordashapp.model.Restaurant;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RestaurantAdapter.OnFavoriteRestaurantListener {
 
     public static final String DOORDASH_LAT = "37.422740";
     public static final String DOORDASH_LNG = "-122.139956";
     private TextView resultTextView;
     private ProgressBar progressBarView;
     private ArrayList<Restaurant> restaurantList;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +46,18 @@ public class MainActivity extends AppCompatActivity {
 
         View appBarMain = findViewById(R.id.app_bar_main);
         View contentMainView = appBarMain.findViewById(R.id.content_main);
-        resultTextView = (TextView) contentMainView.findViewById(R.id.textView);
         progressBarView = (ProgressBar) contentMainView.findViewById(R.id.progressBar);
+        recyclerView = (RecyclerView) findViewById(R.id.restaurantRecyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
 
         URL url = DoorDashApi.buildUrl(DOORDASH_LAT, DOORDASH_LNG);
-
         new RestaurantQueryTask().execute(url);
+    }
+    @Override
+    public void onStarImagePressed(int position) {
+        Toast.makeText(this, position + " id " + restaurantList.get(position).getName(), Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -82,11 +92,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String json) {
             if (json != null && !json.isEmpty()) {
-
                 progressBarView.setVisibility(View.INVISIBLE);
                 restaurantList = DoorDashApi.getRestaurantListFromJson(json);
-                resultTextView.setText(restaurantList.toString());
-
+                RestaurantAdapter restaurantAdapter = new RestaurantAdapter(restaurantList);
+                recyclerView.setAdapter(restaurantAdapter);
             }
         }
     }
